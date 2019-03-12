11aef92f76588a626c0043413a9f6ec09ec2d596
Higuchi-Kazuya <44736418+Higuchi-Kazuya@users.noreply.github.com>
2019-03-13 00:03:48 +0900

    Update コード.js
    テスト1回目に対する修正

--- コード.js
+++ コード.js
@@ -4,6 +4,12 @@ var LIMIT_ROW = 10;
 //スプレットシートのURL
 var SHEET_URL = "https://docs.google.com/spreadsheets/d/14gF_mGaMyxqxowmRM9LTySC4jmbeDqpjdXLIOX6CLxY/edit#gid=0"; 
 
+//一覧検索用の単語
+var ALL_KEYWORD = "#一覧";
+
+//表示する場合のフラグ
+var SHOW_FLAG = 1;
+
 //IDをもっている列（A列を0とする）
 var ID_COL = 0;
 
@@ -16,8 +22,23 @@ var URL_COL = 2;
 //キーワードを持っている列
 var KEYWORD_COL = 3;
 
+//表示するかを制御するフラグを持っている列
+var SHOW_FLAG_COL = 4;
+
 //スプレットシートから持ってきたい列の数(現状はA~Dで4　仕様変更に備えて実装)
-var MAX_ROW = 4;
+var MAX_ROW = 5;
+
+//チャットボットの文字数制限（4096不変）
+var LIMIT_MAX_LENGTH = 4096;
+
+//検索結果無し時のメッセージ
+var MSG_ERROR_NOTHING = "検索対象がありませんでした。";
+
+//チャットボット文字数制限抵触時のメッセージ
+var MSG_ERROR_OVERLIMIT = "検索結果が多すぎます。\n検索キーワードを変更してください。";
+
+//不明なエラー発生時
+var MSG_ERROR_OTHER = "エラーが発生しました。";
 
 /**
 * Responds to a MESSAGE event in Hangouts Chat.
@@ -25,62 +46,90 @@ var MAX_ROW = 4;
 * @param {Object} event the event object from Hangouts Chat
 */
 function onMessage(event) {
-  var message =  getManualURL(event.message.text);
+  var message = "";
+  var leng = 0;
+  if (getJudge(event.message.text, "天気", 3)){
+    message = weatherForecast(event.message.text);
+  }else{
+    message = getManualURL(event.message.text,event.eventTime.seconds);
+  }
   return { "text": message };
 }  
 
 /**
-* Responds to an ADDED_TO_SPACE event in Hangouts Chat.
-*
-* @param {Object} event the event object from Hangouts Chat
-*/
+ * Responds to an ADDED_TO_SPACE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
 function onAddToSpace(event) {
   var message = "";
-  
+
   if (event.space.type == "DM") {
     message = "Thank you for adding me to a DM, " + event.user.displayName + "!";
   } else {
     message = "Thank you for adding me to " + event.space.displayName;
   }
-  
+
   if (event.message) {
     // Bot added through @mention.
     message = message + " and you said : \"" + event.message.text + "\"";
   }
-  
+
   return { "text": message };
 }
 
 /**
-* Responds to a REMOVED_FROM_SPACE event in Hangouts Chat.
-*
-* @param {Object} event the event object from Hangouts Chat
-*/
+ * Responds to a REMOVED_FROM_SPACE event in Hangouts Chat.
+ *
+ * @param {Object} event the event object from Hangouts Chat
+ */
 function onRemoveFromSpace(event) {
   console.info("Bot removed from ", event.space.name);
 }
 
-//メイン処理(検索する文字)
-function getManualURL(eventMessage) {
-  try{  
+function weatherForecast(text) {
+  var strURL = "http://weather.livedoor.com/forecast/webservice/json/v1?city=";
+  var city = "130010";
+  if(getJudge(text,"新潟",4)){
+    city = "150010";
+  }
+  //var response = UrlFetchApp.fetch("http://weather.livedoor.com/forecast/webservice/json/v1?city=130010"); //URL+cityID
+  var response = UrlFetchApp.fetch(strURL + city);
+  var json = JSON.parse(response.getContentText());
+  var message = "";
+  message = json["title"] + "\n";
+  
+  
+  message = message + "今日(" + Utilities.formatDate( new Date(json["forecasts"][0]["date"]), 'Asia/Tokyo', 'yyyy/MM/dd') + ")の天気： " + json["forecasts"][0]["telop"] + "\n";
+  message = message + "明日(" + Utilities.formatDate( new Date(json["forecasts"][1]["date"]), 'Asia/Tokyo', 'yyyy/MM/dd') + ")の天気： " + json["forecasts"][1]["telop"] + "\n";
+  message = message + json["description"]["text"] + "\n\n";
+  message = message + "予報発表時間：" + Utilities.formatDate(new Date(json["publicTime"]), 'Asia/Tokyo', 'yyyy/MM/dd HH:mm');
+  return  message;
+}
+
+//検索する文字,表示する最大行
+function getManualURL(eventMessage,eventTime) {
+  //返答メッセージ
+  var message = "";
+  try{
     //検索ワード格納
     var target = String(eventMessage);
     
     //検索タイプ 1:通常検索 2:一覧表示 3:ID検索
     var type = 1;
     
-    //返答メッセージ
-    var message = "";
-    
     //AND検索用
     var listTarget = new Array();
     
-    if(getJudge(target,"#一覧",2)){// #一覧　の場合は一覧表示する。
+    if(getJudge(target, ALL_KEYWORD,2)){// #一覧　の場合は一覧表示する。
       type = 2;
     }else if(getJudge(target,"#",3)){ //　#　が付いていたら検索先をA列にする
       type = 3;
     }else{// それ以外は通常検索
       //スペース毎に検索キーワードを分割
+      while(getJudge(target,",",1)){
+        target = target.replace(","," ");
+      }
       while(getJudge(target,"　",1)){
         target = target.replace("　"," ");
       }
@@ -91,20 +140,33 @@ function getManualURL(eventMessage) {
     }  
     
     // スプレットシート取得
-    var spreadSheet = SpreadsheetApp.openByUrl(SHEET_URL);
-    
+    try{
+      var spreadSheet = SpreadsheetApp.openByUrl(SHEET_URL);
+    }catch(e){
+      throw ("URLエラー \n" + e);
+    }
     //シート取得
-    var sheet = spreadSheet.getActiveSheet();
-    
+    try{
+      var sheet = spreadSheet.getSheetByName("一覧");
+    }catch(e){
+      throw ("シート取得エラー \n" + e);
+    }
     //シートの最終行取得
     var rastRow = sheet.getLastRow();
     
     // (1行,1列目,　から　最終行,4列目　を取得する)
-    var range = sheet.getRange(1,1,rastRow,MAX_ROW);
+    try{
+       var range = sheet.getRange(1,1,rastRow,MAX_ROW);
+    }catch(e){
+      throw ("セル取得エラー \n" + e);
+    }
     
     //取得したデータを配列に入れる
-    var values = range.getValues();
-    
+    try{
+      var values = range.getValues();
+    }catch(e){
+      throw ("配列格納エラー \n" + e);
+    }
     //ヒット件数
     var count = 0;
     
@@ -121,20 +183,20 @@ function getManualURL(eventMessage) {
       //一覧をループで回る
       for(var i = 1; i<rastRow; i++){
         //一覧表示またはターゲットが一覧にある場合、リストに入れる
-        if(type == 2 || getResult(values[i],listTarget,1)){
+        if(getJudge(values[i][SHOW_FLAG_COL],SHOW_FLAG,2) &&(type == 2 || getResult(values[i],listTarget,1))){
           count++;
           listID.push(String(values[i][ID_COL]));
           listFile.push(String(values[i][FILE_COL]));
           listURL.push(String(values[i][URL_COL]));
-        }
-        
+        }    
       }
     }else if(type == 3){
       //A列を検索の対象にする
       for(var i = 1; i<rastRow; i++){
         //ターゲットが一覧にある場合リストに入れ、以降の検索を中止
-        if(getJudge(String(values[i][ID_COL]),target,2)){
+        if(getJudge(values[i][SHOW_FLAG_COL],SHOW_FLAG,2) && getJudge(String(values[i][ID_COL]),target,2)){
           count=-1;
+          listID.push(String(values[i][ID_COL]));
           listFile.push(String(values[i][FILE_COL]));
           listURL.push(String(values[i][URL_COL]));
           break;
@@ -145,7 +207,7 @@ function getManualURL(eventMessage) {
     if(count == -1 ){ //ID検索で見つかった場合
       message = "検索結果\n\n" +listFile[0]+ "\n" + listURL[0]; 
     }else if(count == 0){ //検索結果が0件の場合
-      message = "検索対象がありませんでした。";
+      message = MSG_ERROR_NOTHING;
     }else if(count > 0 && count <= LIMIT_ROW){　//検索結果が１件以上かつ限界値以下の場合
       if(type == 2){　//一覧表示の場合
         message = "一覧 \n\n";
@@ -166,26 +228,62 @@ function getManualURL(eventMessage) {
       }
       message = message + "\n" + "#で始まる５桁のIDで再度検索するとURLを表示します。";
     }
-    
+    var logSheet = spreadSheet.getSheetByName("検索ログ");
+    var logLastRow =logSheet.getLastRow();   
+    if(message.length > LIMIT_MAX_LENGTH){
+      message = MSG_ERROR_OVERLIMIT;
+    }
   }catch(e){
-    message = "エラーが発生しました。\n" + e;
+    message = MSG_ERROR_OTHER +"\n" + e;
   }finally{
     //messageをreturn
+    setLog(logSheet,message,listID,eventMessage,eventTime)
     return message;
   }
 }
 
-//AND検索用(スプレットーシートから得た任意の1行,ANDで検索する文字配列,検索するタイプ(部分一致が基本))
-//検索する文字配列の単語がすべて一行の中にあればtrue、一つでもない単語があればfalseを返す。
+/*
+** AND検索用(スプレットーシートから得た配列,ANDで検索する文字配列,検索するタイプ(部分一致が基本))
+*/
 function getResult(values,listTarget,type){
   for(var i=0;i<listTarget.length;i++){ //文字配列の個数だけ検索する。
     if(!getJudge(String(values[FILE_COL]),listTarget[i],type)　&& !getJudge(String(values[KEYWORD_COL]),listTarget[i],type)){
-      //B列にもD列にも検索文字列がない場合の処理
+    //B列にもD列にも検索文字列がない場合の処理
       return false;//ANDの条件を満たさないのでfalseでreturn
     }
   }
   return true;//ANDの条件を満たしたのでtrueでreturn
 }
+
+//ログ出力関数
+function setLog(logSheet,message,listID,eventMessage,eventTime){
+  var logLastRow = logSheet.getLastRow();
+  logSheet.getRange(logLastRow+1,1).setValue(String(eventMessage));
+  var data = new Date((1+eventTime)*1000);
+  var year = data.getFullYear();
+  var month = ('0' + (data.getMonth()+1)).slice(-2);
+  var date = ('0' + data.getDate()).slice(-2);
+  var hours = ('0' + data.getHours()).slice(-2);
+  var minutes = ('0' + data.getMinutes()).slice(-2);
+  var seconds = ('0' + data.getSeconds()).slice(-2);
+  var output = "";
+  logSheet.getRange(logLastRow+1,2).setValue(year + "/" + month + "/" + date + " " + hours + ":" + minutes + ":" + seconds);
+  if (getJudge(message,MSG_ERROR_NOTHING,2)){
+    logSheet.getRange(logLastRow+1,3).setValue(MSG_ERROR_NOTHING);
+  }else if(getJudge(message,MSG_ERROR_OVERLIMIT,2)){
+    logSheet.getRange(logLastRow+1,3).setValue(MSG_ERROR_OVERLIMIT);
+  }else if(getJudge(message,MSG_ERROR_OTHER,3)){
+    logSheet.getRange(logLastRow+1,3).setValue(message);
+  }else{
+    for(var i=0;i<listID.length;i++){
+      output = output + listID[i] +",";
+    }
+    output = output.substr(0,output.length-1)
+    logSheet.getRange(logLastRow+1,3).setValue(output);
+  }
+  return;
+}
+
 
 //文字列検索関数(検索対象文字列,検索文字,検索するタイプ)
 function getJudge(text,target,type){
